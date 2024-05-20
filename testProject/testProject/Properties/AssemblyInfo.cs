865d77033e02ae9391dc04890bc7ed7ec1bf1b8f
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-05-20 20:58:13 +0600

    test

--- testProject/testProject/Properties/AssemblyInfo.cs
+++ testProject/testProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hello")]
+[assembly: AssemblyTitle("testProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Hello")]
+[assembly: AssemblyProduct("testProject")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b56e2199-5b1a-4e33-a298-1a62c21f2631")]
+[assembly: Guid("544bc673-705b-4d2a-90f1-2581ff5848e2")]
 
 // Version information for an assembly consists of the following four values:
 //
