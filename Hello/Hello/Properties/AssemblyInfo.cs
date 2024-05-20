145654024e6dc926fcf8038564b922a03ee97cad
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-05-20 21:03:45 +0600

    test

--- Hello/Hello/Properties/AssemblyInfo.cs
+++ Hello/Hello/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4380d6bf-cc41-40e6-a1d0-66468c22bf6b")]
+[assembly: Guid("0aa91c31-ffae-464f-b74d-2b028140e001")]
 
 // Version information for an assembly consists of the following four values:
 //
