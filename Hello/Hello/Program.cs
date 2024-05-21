c731a50058120c5e22216410526e5fe7bfddddc6
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2024-05-21 23:57:54 +0600

    hh

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -10,6 +10,27 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            /*this is a comment*/
+            int rakin = 190; //integer variable
+            /*MULTI-LINE COMMENT
+       
+             ** Variables
+            integer - int - 4 bytes
+            floating point number - float -4 bytes
+            character - char = ''; - 2 bytes
+            boolean - bool isGreat = true/false 1bit
+            string inp = "harry" 
+            */
+
+
+            Console.Write("Hello World");
+            Console.WriteLine("Hello Rakin");
+            Console.WriteLine("Hello World" + rakin);
+            Console.ReadLine();
+
+            //new input
+            string inp = Console.ReadLine(); // Readline()
+            Console.WriteLine(inp);
         }
     }
 }
